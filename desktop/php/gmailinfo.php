46692d95856164fe36c1bc121a0668adf109c847
cazeaux <stephane.cazeaux@casper-online.net>
2015-08-18 23:47:08 +0200

    v1.2

--- desktop/php/gmailinfo.php
+++ desktop/php/gmailinfo.php
@@ -6,7 +6,7 @@ sendVarToJS('eqType', 'gmailinfo');
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2">
+    <div class="col-lg-2 col-md-3 col-sm-4"">
         <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
                 <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un équipement}}</a>
@@ -20,20 +20,48 @@ sendVarToJS('eqType', 'gmailinfo');
         </div>
     </div>
 
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend>{{Mes équipements Gmailinfo}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+				<center>
+					<i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+				</center>
+				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+			</div>
+        <?php
+		foreach ($eqLogics as $eqLogic) {
+			echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+			echo "<center>";
+			echo '<img src="plugins/pushbullet/doc/images/gmailinfo.png" height="105"  />';
+			echo "</center>";
+			echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+			echo '</div>';
+		}
+        ?>
+		</div>
+
+	</div>
+	
+	
     <div class="col-lg-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
         <form class="form-horizontal">
             <fieldset>
-                <legend>{{Général}}</legend>
+				<legend>
+					<i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
+					<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
+					<a class="btn btn-xs btn-default pull-right eqLogicAction" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
+				</legend>
                 <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Nom de l'équipement GMail}}</label>
-                    <div class="col-lg-3">
+                    <label class="col-sm-3 control-label">{{Nom de l'équipement GMail}}</label>
+                    <div class="col-sm-3">
                         <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
                         <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement GMail}}"/>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-2 control-label" >{{Objet parent}}</label>
-                    <div class="col-lg-3">
+                    <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                    <div class="col-sm-3">
                         <select class="eqLogicAttr form-control" data-l1key="object_id">
                             <option value="">{{Aucun}}</option>
                             <?php
@@ -45,26 +73,23 @@ sendVarToJS('eqType', 'gmailinfo');
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Activer}}</label>
-                    <div class="col-lg-1">
-                        <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>
-                    </div>
-                    <label class="col-lg-2 control-label">{{Visible}}</label>
-                    <div class="col-lg-1">
-                        <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
-                    </div>
+					<label class="col-sm-3 control-label"></label>
+					<div class="col-sm-9">
+						<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+						<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+					</div>
                 </div>
                 <div class="form-group">
-                    <label class="col-md-2 control-label">{{Compte GMail}}</label>
+                    <label class="col-sm-3 control-label">{{Compte GMail}}</label>
                     <div class="col-md-3">
                         <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="email" placeholder="@gmail.com"/>
                     </div>
                 </div>
-        <div class="alert alert-info">
-            {{Mot de passe : Si vous avez activé la vérification en 2 étapes du GMail, utilisez un mot de passe d'application spécifique. Sinon, utilisez le mot de passe de compte.}}
-        </div>
+				<div class="alert alert-info">
+					{{Mot de passe : Si vous avez activé la vérification en 2 étapes du GMail, utilisez un mot de passe d'application spécifique. Sinon, utilisez le mot de passe de compte.}}
+				</div>
                 <div class="form-group">
-                    <label class="col-md-2 control-label">{{Mot de passe}}</label>
+                    <label class="col-sm-3 control-label">{{Mot de passe}}</label>
                     <div class="col-md-3">
                         <input type="password" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="password" placeholder=""/>
                     </div>
